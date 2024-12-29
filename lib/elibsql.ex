6783d5f5a39b2839c5eda11d580394ec11ffddb4
Michael Wang <thelowkeypanda@gmail.com>
2024-12-29 15:32:57 -0800

    i dont even know what i did

--- lib/elibsql.ex
+++ lib/elibsql.ex
@@ -2,4 +2,11 @@ defmodule ElibSQL do
   @moduledoc """
   Documentation for `ElibSQL`.
   """
+
+  @doc"""
+  Start link
+  """
+  def start_link(opts) do
+    DBConnection.start_link(ElibSQL.Protocol, opts)
+  end
 end
