77ce631e560aa73b455bf6ad0b5a0613ef2eec15
Michael Wang <thelowkeypanda@gmail.com>
2025-01-05 10:16:18 -0800

    add some varint decoding

--- lib/protobuf/protobuf.ex
+++ lib/protobuf/protobuf.ex
@@ -7,4 +7,53 @@ defmodule ElibSQL.Protobuf do
   @type t :: %__MODULE__{
           messages: %{binary() => ElibSQL.Protobuf.Parser.message()}
         }
+  
+  @doc """
+  Decodes a binary into a map representing the corresponding
+  message type
+  """
+  @spec decode(t(), binary(), binary()) :: {:ok, map()} | {:err, any()}
+  def decode(state, data, message_type) do
+    type = Map.get(state, message_type) 
+  end
+  
+  @doc """
+  Encodes a map into the binary representing the corresponding 
+  message type
+  """
+  @spec encode(t(), map(), binary()) :: {:ok, binary()} | {:err, any()}
+  def encode(state, data, message_type) do
+  end
+  
+  @doc """
+  Decodes a binary to extract the first varint (returning it in 
+  big endian) and also returns the remaining binary
+  """
+  @spec decode_varint(binary()) :: {:ok, bitstring(), binary()} | {:err, any()}
+  def decode_varint(<<0::1, data::bits-size(7), rest::binary>>, acc) do
+    {:ok, <<data::bitstring, acc::bitstring>>, rest}
+  end
+  def decode_varint(<<1::1, data::bits-size(7), rest::binary>>, acc \\ <<>>) do
+    decode_varint(rest, <<data::bitstring, acc::bitstring>>)
+  end
+
+  @doc """
+  Converts a 2s complement bitstring to integer
+  """
+  def bitstring_to_int(<<>>, acc), do: acc
+  def bitstring_to_int(<<first::1, rest::bitstring>>, acc \\ 0) do 
+    bitstring_to_int(rest, acc * 2 + first)
+  end
+
+  @doc """
+  Converts a ZigZag binary to signed integer
+  """
+  def binary_to_sint(binary) do
+  end
+  
+  @doc """
+  Converts a binary to floating point (either double or float)
+  """
+  def binary_to_decimal(binary) do
+  end
 end
--- test/elibsql_test.exs
+++ test/elibsql_test.exs
@@ -82,4 +82,14 @@ defmodule ElibSQLTest do
 
     assert expected == tokens
   end
+
+  test "decode varint" do
+    input = << 0x96, 0x1 >> 
+    assert ElibSQL.Protobuf.decode_varint(input) == {:ok, <<2, 22::6>>, <<>>}
+  end
+
+  test "bitstring to int" do
+    input = <<2, 22::6>>
+    assert ElibSQL.Protobuf.bitstring_to_int(input) == 150
+  end
 end
