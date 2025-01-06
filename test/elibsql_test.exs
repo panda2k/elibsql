a171436f95e9b1a7d1a0df206ab2177692c54a1d
Christopher Nguyen <chris@Christophers-MacBook-Air.local>
2025-01-06 14:18:49 -0800

    fixed requests

--- test/elibsql_test.exs
+++ test/elibsql_test.exs
@@ -2,18 +2,24 @@ defmodule ElibSQLTest do
   use ExUnit.Case
   doctest ElibSQL
 
-  test "execute dummy statement" do
-
+  test "execute simple select statement" do
     Dotenvy.source!([".env", System.get_env()])
-    
+
     hostname = Dotenvy.env!("HOSTNAME", :string)
     port = 443
     timeout = 5000
     token = Dotenvy.env!("TOKEN", :string)
 
-    {:ok, pid} = ElibSQL.start_link([hostname: hostname, port: port, timeout: timeout, token: token])
-    ElibSQL.prepare_execute(pid, "SELECT ?", [42], [])
-    |> IO.inspect
-  end
+    {:ok, pid} =
+      ElibSQL.start_link(hostname: hostname, port: port, timeout: timeout, token: token)
 
+    {:ok, query, result} = ElibSQL.prepare_execute(pid, "SELECT ?", [42], [])
+    assert %ElibSQL.Query{} = query
+    assert %ElibSQL.Result{} = result
+
+    assert result == %ElibSQL.Result{
+             columns: [%{"decltype" => nil, "name" => "?"}],
+             rows: [[%{"type" => "integer", "value" => "42"}]]
+           }
+  end
 end
